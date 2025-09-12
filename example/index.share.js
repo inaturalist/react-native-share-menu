7c6ca49e5047fbb1d0067ce9d1063813a588682e
Johannes Klein <johannes.t.klein@gmail.com>
2025-09-12 03:29:53 +0200

    Update index.share.js

--- example/index.share.js
+++ example/index.share.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import Share from './Share';
 
 AppRegistry.registerComponent('ShareMenuModuleComponent', () => Share);
