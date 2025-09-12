4ceee6b7856b23cb1c14ffa33c97c4981bbd5052
Johannes Klein <johannes.t.klein@gmail.com>
2025-09-12 11:39:42 +0200

    fix: add old headers back in again

--- ios/ShareMenu-Bridging-Header.h
+++ ios/ShareMenu-Bridging-Header.h
@@ -1,2 +1,8 @@
+#ifndef ShareMenu_Bridging_Header_h
+#define ShareMenu_Bridging_Header_h
+
 #import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
+#import <React/RCTEventEmitter.h>
+
+#endif
