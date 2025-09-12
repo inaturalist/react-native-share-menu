eaeb15daa25bf7886d353b6dd12fe28754ce3ade
Johannes Klein <johannes.t.klein@gmail.com>
2025-09-12 03:13:41 +0200

    Update ShareMenu-Bridging-Header.h

--- ios/ShareMenu-Bridging-Header.h
+++ ios/ShareMenu-Bridging-Header.h
@@ -1,7 +1,2 @@
-#ifndef ShareMenu_Bridging_Header_h
-#define ShareMenu_Bridging_Header_h
-
 #import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-#endif
+#import <React/RCTViewManager.h>
