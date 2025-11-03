7898d71cf22375d183a6026cc69111e699818c1e
Johannes Klein <johannes.t.klein@gmail.com>
2025-11-03 10:18:09 +0100

    Update example app to react-native 0.80.x (#6)
    * chore: update example app to react-native 0.79
    
    * chore: update example app to react-native 0.80

--- example/ios/Test/AppDelegate.mm
+++ example/ios/Test/AppDelegate.mm
@@ -4,6 +4,9 @@
 #import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
 #import <RNShareMenu/ShareMenuManager.h>
 
+@interface AppDelegate ()
+@end
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)app
@@ -17,13 +20,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"example";
+  self.reactNativeFactory = [[RCTReactNativeFactory alloc] initWithDelegate:self];
   self.dependencyProvider = [RCTAppDependencyProvider new];
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+
+  [self.reactNativeFactory startReactNativeWithModuleName:@"example"
+                                                 inWindow:self.window
+                                        initialProperties:@{}
+                                            launchOptions:launchOptions];
+
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
@@ -34,7 +41,7 @@
 - (NSURL *)bundleURL
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  return [RCTBundleURLProvider.sharedSettings jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
